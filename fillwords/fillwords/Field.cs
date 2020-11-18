d11309aacbb1b8ecc1fe303a5f2fe00bddebd29e
Yakov <raptoroidinbox@gmail.com>
2020-11-19 00:30:42 +0500

    added field class

---
+++ fillwords/fillwords/Field.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fillwords
+{
+    class Field
+    {
+        static public int height, width;
+
+        static public void PrintField()
+        {
+            static void PrintRow(int times, string inp)
+            {
+                for (int i = 0; i < times; i++)
+                    Console.Write(inp);
+            }
+
+            static void PrintField(char[,] letters)
+            {
+
+                Console.Write(new string('\n', (Console.WindowHeight - 1 - height * 2) / 2));
+
+                for (int j = 0; j < height; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write($"{new string(' ', (Console.WindowWidth - 1 - width * 4) / 2)}╔");
+                        PrintRow(width - 1, "═══╦");
+                        Console.Write("═══╗\n");
+                    }
+                    else
+                    {
+                        Console.Write($"{new string(' ', (Console.WindowWidth - 1 - width * 4) / 2)}╠");
+                        PrintRow(width - 1, "═══╬");
+                        Console.Write("═══╣\n");
+                    }
+
+
+                    for (int i = 0; i < width; i++)
+                    {
+                        if (i == 0)
+                            Console.Write($"{new string(' ', (Console.WindowWidth - 1 - width * 4) / 2)}║ {letters[j, i]} ║");
+                        else Console.Write($" {letters[j, i]} ║");
+                    }
+
+                    Console.Write("\n");
+
+                    if (j == height - 1)
+                    {
+                        Console.Write($"{new string(' ', (Console.WindowWidth - 1 - width * 4) / 2)}╚");
+                        PrintRow(width - 1, "═══╩");
+                        Console.Write("═══╝\n");
+                    }
+                }
+
+                Console.Write(new string('\n', (Console.WindowHeight - 1 - height * 2) / 2));
+            }
+        }
+        
+    }
+}
